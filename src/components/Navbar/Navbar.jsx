c137a106990f8b165f1161259f8c337fe078245e
Satvikjoshi17 <122602881+Satvikjoshi17@users.noreply.github.com>
2025-09-12 21:58:45 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,79 +1,89 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { signOutUser } from '../../redux/actionCreators/authActionCreator';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
   const { isAuthenticated, user } = useSelector((state) => state.auth);
   const location = useLocation();
-  const dispatch = useDispatch();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
-    <nav className='navbar'>
-      <a href="https://www.dauniv.ac.in/" target='_blank' className='footer-text ' style={{ marginLeft: 50 + 'px' }}>
-        <img
-          src="/assets/favicon.ico"
-          alt="Davv Logo"
-          className='footer-logo'
-        /> IIPS
+    <nav className="navbar">
+      {/* Brand / Logo */}
+      <a
+        href="https://www.dauniv.ac.in/"
+        target="_blank"
+        rel="noreferrer"
+        className="footer-text"
+        style={{ marginLeft: '50px', display: 'flex', alignItems: 'center' }}
+      >
+        <img src="/assets/favicon.ico" alt="Davv Logo" className="footer-logo" />
+        <span style={{ marginLeft: '8px', fontWeight: 'bold' }}>IIPS</span>
       </a>
-      <div className='navbar-container'>
-        <div className='navbar-links'>
-          {location.pathname === '/' && (
-            <Link className='navbar-brand' to={isAuthenticated ? '/dashboard' :'/login'}>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? 'open' : ''}`} />
+        <div className={`bar ${menuOpen ? 'open' : ''}`} />
+        <div className={`bar ${menuOpen ? 'open' : ''}`} />
+      </div>
+
+      {/* Links */}
+      <div className={`navbar-container ${menuOpen ? 'open' : ''}`}>
+        <div className="navbar-links">
+          {location.pathname === '/' ? (
+            <Link className="navbar-brand" to={isAuthenticated ? '/dashboard' : '/login'} onClick={closeMenu}>
               Academics
             </Link>
-          )}
-          {location.pathname !== '/' && (
-            <Link className='navbar-brand' to='/'>
+          ) : (
+            <Link className="navbar-brand" to="/" onClick={closeMenu}>
               Home
             </Link>
           )}
-          <Link className='navbar-brand' to='/placement'>
+
+          <Link className="navbar-brand" to="/placement" onClick={closeMenu}>
             Placement
           </Link>
-          <Link className='navbar-brand' to='/events'>
+          <Link className="navbar-brand" to="/events" onClick={closeMenu}>
             Events
           </Link>
-
-
-          <Link className='navbar-brand' to='/aboutus'>
+          <Link className="navbar-brand" to="/aboutus" onClick={closeMenu}>
             About
           </Link>
-
-          <Link className='navbar-brand' to='/our-contributers'>
+          <Link className="navbar-brand" to="/our-contributers" onClick={closeMenu}>
             Contributors
           </Link>
         </div>
-      </div>
-      <ul className='navbar-nav'>
-        {isAuthenticated ? (
-          <>
 
-            <li className='nav-item'>
-              <p className='user-welcome'>
-                <span className='user-name'>{user.displayName?.toUpperCase()}</span>
-                <img className='user-icon' src="/assets/user-icon.png" alt="Admin" />
+        {/* User Section */}
+        <ul className="navbar-nav">
+          {isAuthenticated ? (
+            <li className="nav-item">
+              <p className="user-welcome">
+                <span className="user-name">{user.displayName?.toUpperCase()}</span>
+                <img className="user-icon" src="/assets/user-icon.png" alt="Admin" />
               </p>
             </li>
-
-          </>
-        ) : (
-          <>
-            <li className='nav-item'>
-              <Link className='btnNew' to='/login'>
-                Sign In
-
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='btnNew' to='/Register'>
-                Sign Up
-
-              </Link>
-            </li>
-          </>
-        )}
-      </ul>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link className="btnNew" to="/login" onClick={closeMenu}>
+                  Sign In
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="btnNew" to="/Register" onClick={closeMenu}>
+                  Sign Up
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
